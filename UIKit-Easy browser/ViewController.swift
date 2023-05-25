e754f71ee3dd9a773fb10e6e04240e09af37e128
Akshay Mahajan <maha0134@algonquinlive.com>
2023-05-25 11:31:11 -0400

    make webview functional with action sheet

--- UIKit-Easy browser/ViewController.swift
+++ UIKit-Easy browser/ViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKNavigationDelegate {
+	var webView: WKWebView!
+	
+	override func loadView() {
+		webView = WKWebView()
+		webView.navigationDelegate = self
+		view = webView
+	}
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+		let url = URL(string: "https://portfolio-akshay-mahajan.netlify.app/")!
+		webView.load(URLRequest(url: url))
+		webView.allowsBackForwardNavigationGestures = true
 	}
-
-
+	@objc func openTapped() {
+		let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
+		ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+		ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
+		ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+		ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+		
+		present(ac, animated: true)
+	}
+	
+	func openPage(action: UIAlertAction) {
+		let url = URL(string: "https://" + action.title!)!
+		webView.load(URLRequest(url: url))
+	}
+	func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+		title = webView.title
+	}
 }
 
