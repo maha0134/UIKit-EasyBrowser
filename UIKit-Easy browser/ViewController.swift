e16eb0906d5a846400faa429b7142802a84b7f43
Akshay Mahajan <maha0134@algonquinlive.com>
2023-06-16 12:23:06 +0530

    add progress bar

--- UIKit-Easy browser/ViewController.swift
+++ UIKit-Easy browser/ViewController.swift
@@ -10,6 +10,7 @@ import WebKit
 
 class ViewController: UIViewController, WKNavigationDelegate {
 	var webView: WKWebView!
+	var progressView: UIProgressView!
 	
 	override func loadView() {
 		webView = WKWebView()
@@ -20,6 +21,19 @@ class ViewController: UIViewController, WKNavigationDelegate {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+		
+		let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+		let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+		
+		progressView = UIProgressView(progressViewStyle: .default)
+		progressView.sizeToFit()
+		let progressButton = UIBarButtonItem(customView: progressView)
+		
+		toolbarItems = [progressButton, spacer, refresh]
+		navigationController?.isToolbarHidden = false
+		
+		webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+		
 		let url = URL(string: "https://portfolio-akshay-mahajan.netlify.app/")!
 		webView.load(URLRequest(url: url))
 		webView.allowsBackForwardNavigationGestures = true
@@ -38,8 +52,14 @@ class ViewController: UIViewController, WKNavigationDelegate {
 		let url = URL(string: "https://" + action.title!)!
 		webView.load(URLRequest(url: url))
 	}
+	
 	func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
 		title = webView.title
+	}
+	override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+		if keyPath == "estimatedProgress" {
+			progressView.progress = Float(webView.estimatedProgress)
+		}
 	}
 }
 
